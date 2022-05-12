6b05852422f295e7114dd37aede417b81f4b4b25
Deepak <deepakkudc16102000@gmail.com>
2022-05-13 01:26:21 +0530

    Firebase configuration added

--- js/download-pdf.js
+++ js/download-pdf.js
@@ -1,3 +1,36 @@
+function downloadPDF() {
+
+    var HTML_Width = $("#card").width();
+    var HTML_Height = $("#card").height();
+    var top_left_margin = 15;
+    var PDF_Width = HTML_Width + (top_left_margin * 2);
+    var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+    var canvas_image_width = HTML_Width;
+    var canvas_image_height = HTML_Height;
+
+    var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+
+    html2canvas($("#card")[0], { allowTaint: true }).then(function (canvas) {
+        canvas.getContext('2d');
+
+        console.log(canvas.height + "  " + canvas.width);
+
+
+        var imgData = canvas.toDataURL("image/jpeg", 1.0);
+        var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+        pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+
+
+        for (var i = 1; i <= totalPDFPages; i++) {
+            pdf.addPage(PDF_Width, PDF_Height);
+            pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+        }
+
+        pdf.save("card.pdf");
+    });
+};
+
 // function downloadPDF() {
 //     html2canvas($("#card")[0], {
 //         allowTaint: true
@@ -35,37 +68,3 @@
 
 // }
 
-
-
-function downloadPDF() {
-
-    var HTML_Width = $("#card").width();
-    var HTML_Height = $("#card").height();
-    var top_left_margin = 15;
-    var PDF_Width = HTML_Width + (top_left_margin * 2);
-    var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-    var canvas_image_width = HTML_Width;
-    var canvas_image_height = HTML_Height;
-
-    var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-
-
-    html2canvas($("#card")[0], { allowTaint: true }).then(function (canvas) {
-        canvas.getContext('2d');
-
-        console.log(canvas.height + "  " + canvas.width);
-
-
-        var imgData = canvas.toDataURL("image/jpeg", 1.0);
-        var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-        pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
-
-
-        for (var i = 1; i <= totalPDFPages; i++) {
-            pdf.addPage(PDF_Width, PDF_Height);
-            pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
-        }
-
-        pdf.save("card.pdf");
-    });
-};
---
+++ js/init-firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAs3WPwc0PehBkIlOfNDxrJRJX1Jnx19CE",
+    authDomain: "breviewqr-687aa.firebaseapp.com",
+    projectId: "reviewqr-687aa",
+    databaseURL: "https://reviewqr-687aa-default-rtdb.firebaseio.com",
+    storageBucket: "reviewqr-687aa.appspot.com",
+    messagingSenderId: "246845270843",
+    appId: "1:246845270843:web:c1cfca337f426653a8a51e"
+};
+firebase.initializeApp(firebaseConfig);
+console.log("Firebase initialized");
+const db = firebase.firestore();
+const auth = firebase.auth();
